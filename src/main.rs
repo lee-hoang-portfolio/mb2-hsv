a7bfccb1a7c225d235416d09241072182bb741a3
Lee Hoang <leehoang97@gmail.com>
2026-02-22 20:00:42 -0800

    add crate doc links to main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
+// cortex_m_rt crate
+// define the entry point
+// docs: https://docs.rs/cortex-m-rt/0.7.5/cortex_m_rt/
 use cortex_m_rt::entry;
+
+// Microbit crate
+// docs: https://docs.rs/microbit-v2/0.16.0/microbit/
 use microbit::board::Board;
 
 // =======================================================
